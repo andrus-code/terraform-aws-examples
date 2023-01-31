4f95f47ba2165cb311515de39325936ed09bc933
Andres <salamancaclaudioandres@gmail.com>
2023-01-31 10:58:33 -0300

    update 01-hello-world

--- 01-hello-world/main.tf
+++ 01-hello-world/main.tf
@@ -1,11 +1,11 @@
 # Configure the AWS provider
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-785db401"
+  ami           = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
 }
 
