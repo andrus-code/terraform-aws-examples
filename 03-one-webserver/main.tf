dff92f5f90d5b98108915a25331e74a35e31ed05
Andres <salamancaclaudioandres@gmail.com>
2023-01-31 11:54:03 -0300

    03-one-webserver

---
+++ 03-one-webserver/main.tf
@@ -0,0 +1,44 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+# Create a Security Group for an EC2 instance
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  
+  ingress {
+    from_port     = 8080
+    to_port       = 8080
+    protocol      = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-00874d747dde814fa"
+  instance_type = "t2.micro"
+  vpc_security_group_ids  = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+   Name = "terraform-example2"
+  }
+}
+
+# Output variable: Public IP address
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
+}
+
+
+
+
