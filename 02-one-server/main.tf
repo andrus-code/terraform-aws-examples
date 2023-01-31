d688cb882c22cc4c9589826337c5f84775caca52
Andres <salamancaclaudioandres@gmail.com>
2023-01-31 11:16:10 -0300

    02-one-server

---
+++ 02-one-server/main.tf
@@ -0,0 +1,16 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-00874d747dde814fa"
+  instance_type = "t2.micro"
+
+  tags = {
+   Name = "terraform-example"
+  }
+}
+
+
